51683a8564cba28229ad7a5042837391fa2f9724
chrl0t <charlotte_i@live.co.uk>
2020-12-07 14:18:34 +0000

    added cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const apiRouter = require('./routers/api-router');
 const {
@@ -8,8 +9,10 @@ const {
   clientErrorHandler,
   PSQLErrorHandler,
   clientErrorMissingInfo,
-  clientErrorUsedUsername,
+  clientErrorUsedUsername
 } = require('./controllers/errors');
+
+app.use(cors());
 
 app.use(express.json());
 app.use('/api', apiRouter);
